bd7125c3430f805f2c8923753cb01a51821b6e30
beregueder <beregueder@gmail.com>
2012-10-16 12:05:41 -0300

    Primeiro commit, dessa aplicação Rails com Twitter BootStrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = 'c1ec8919c8c0cc7adff5a5d4edf9627a1dcdc75f09d736d7b5fb2bb39417908dc086936ff88e29637c4f4601c896fd8ee4c9138817c10933e147387a2e565bb1'
